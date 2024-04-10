5102a7a6ccf937e6c3bb3103f29be24c6b1c77d7
Hoseok Seong <shsshs0125@naver.com>
2024-04-10 17:17:30 +0900

    JwtProvider, Jwt 필터 생성

---
+++ src/main/java/com/example/springsecurityjwt/global/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,62 @@
+package com.example.springsecurityjwt.global.jwt;
+
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.springsecurityjwt.global.security.MyUserDetails;
+import com.example.springsecurityjwt.user.entity.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String prefixJwt = request.getHeader(JwtProvider.ACCESS_TOKEN_HEADER);
+
+        if(prefixJwt == null){
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = prefixJwt.replace(JwtProvider.TOKEN_PREFIX, "");
+
+        try {
+            DecodedJWT decodedJWT = JwtProvider.verifyAccessToken(jwt);
+            Long id = decodedJWT.getClaim("id").asLong();
+            String role = decodedJWT.getClaim("role").asString();
+
+            User user = User.builder().id(id).build();
+            MyUserDetails myUserDetails = new MyUserDetails(user);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    myUserDetails,
+                    myUserDetails.getPassword(),
+                    myUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (SignatureVerificationException sve) {
+            log.error("토큰 검증 실패");
+            throw sve;
+        } catch (TokenExpiredException tee) {
+            log.error("Access 토큰 만료");
+            throw tee;
+        } finally {
+            chain.doFilter(request, response);
+        }
+    }
+}
